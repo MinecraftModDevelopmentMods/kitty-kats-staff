b591dfd67abbcce7f89fcb5abc98df14c89625ce
unknown <simplysilvercube@gmail.com>
2020-10-01 19:38:56 -0500

    Port to 1.15.2

--- src/main/java/com/mcmoddev/kittykatsstaff/KittyKatsStaff.java
+++ src/main/java/com/mcmoddev/kittykatsstaff/KittyKatsStaff.java
@@ -1,65 +1,27 @@
 package com.mcmoddev.kittykatsstaff;
 
-import com.mcmoddev.kittykatsstaff.api.KKSItems;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
+import com.mcmoddev.kittykatsstaff.init.ItemRegistry;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.oredict.OreDictionary;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-import javax.annotation.Nonnull;
-
-@Mod(
-        name = KittyKatsStaff.NAME,
-        modid = KittyKatsStaff.MODID,
-        version = KittyKatsStaff.VERSION,
-        updateJSON = KittyKatsStaff.UPDATE_JSON,
-        certificateFingerprint = "@FINGERPRINT@",
-        acceptedMinecraftVersions = KittyKatsStaff.MC_VERSION
-)
+@Mod(KittyKatsStaff.MODID)
 public class KittyKatsStaff {
 
-    public static final String NAME = "Kitty Kat's Staff";
     public static final String MODID = "kittykatsstaff";
-    public static final String VERSION = "1.0.0";
-    public static final String UPDATE_JSON = "https://raw.githubusercontent.com/MinecraftModDevelopmentMods/kitty-kats-staff/master/update.json";
-    public static final String MC_VERSION = "[1.12, 1.12.2]";
-    public static final Logger LOGGER = LogManager.getLogger(NAME);
 
-    public static final CreativeTabs CREATIVE_TAB = new CreativeTabs(MODID) {
+    public KittyKatsStaff(){
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ItemRegistry.ITEMS.register(modEventBus);
+    }
+
+    public static ItemGroup CREATIVE_TAB = new ItemGroup(KittyKatsStaff.MODID) {
         @Override
-        @Nonnull
         public ItemStack createIcon() {
-            return new ItemStack(KKSItems.STAFF_OF_KITTYS);
+            return new ItemStack(ItemRegistry.KITTY_KATS_STAFF.get());
         }
     };
-
-    @Mod.EventHandler
-    public void onFingerprintViolation(@Nonnull FMLFingerprintViolationEvent event) {
-        LOGGER.warn("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported! Please download the mod from CurseForge for a supported and signed version of the mod.");
-    }
-
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        LOGGER.info("Oooo what's this shiny stick do?");
-    }
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        LOGGER.info("shiny stick: *summons cats* Meow!");
-        //TODO in 1.13 fish no longer use metadata so we can oredict all the fish rather than just two.
-        OreDictionary.registerOre("foodFish", Items.FISH);
-        OreDictionary.registerOre("foodFish", Items.COOKED_FISH);
-    }
-
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-        LOGGER.info("We are ready kat! Lets go play! Meow!");
-    }
 }
--- src/main/java/com/mcmoddev/kittykatsstaff/common/items/ItemKittyKatsStaff.java
+++ src/main/java/com/mcmoddev/kittykatsstaff/common/items/ItemKittyKatsStaff.java
@@ -1,35 +1,38 @@
 package com.mcmoddev.kittykatsstaff.common.items;
 
-import net.minecraft.entity.passive.EntityOcelot;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
+import com.mcmoddev.kittykatsstaff.KittyKatsStaff;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.stats.StatList;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.SoundCategory;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.minecraftforge.oredict.OreDictionary;
 
 import javax.annotation.Nonnull;
-import java.util.List;
 import java.util.Random;
 
 public class ItemKittyKatsStaff extends Item {
 
+    static final int numberOfCharges = 19;
+
     public ItemKittyKatsStaff() {
-        int numberOfCharges = 19;
-        setMaxStackSize(1);
-        setMaxDamage(numberOfCharges + 1);
+        super(new Item.Properties()
+        .group(KittyKatsStaff.CREATIVE_TAB)
+        .maxStackSize(1)
+        .maxDamage(numberOfCharges + 1));
     }
 
     public boolean outOfUses(@Nonnull ItemStack stack) {
-        return stack.getItemDamage() >= (stack.getMaxDamage() - 1);
+        return stack.getDamage() >= (stack.getMaxDamage() - 1);
     }
 
-    private List<ItemStack> allowedItems = null;
+    private Tag<Item> allowedItems = null;
 
     /**
      * Return whether this item is repairable in an anvil.
@@ -38,58 +41,61 @@ public class ItemKittyKatsStaff extends Item {
     public boolean getIsRepairable(ItemStack repairableItem, ItemStack repairMaterial) {
         //Repair with cooked or raw fish.
         if (allowedItems == null) {
-            allowedItems = OreDictionary.getOres("foodFish");
+            allowedItems = ItemTags.FISHES;
         }
 
-        for (int i = 0; i < allowedItems.size(); i++) {
-            if (allowedItems.get(i).getTranslationKey().equals(repairMaterial.getTranslationKey())) {
+        for (int i = 0; i < allowedItems.getAllElements().size(); i++) {
+            if (allowedItems.contains(repairMaterial.getItem())){
                 return true;
             }
         }
         return false;
     }
 
-    public ActionResult<ItemStack> onItemRightClick(@Nonnull World world, @Nonnull EntityPlayer player, EnumHand hand) {
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(@Nonnull World world, @Nonnull PlayerEntity player, Hand hand) {
         ItemStack item = player.getHeldItem(hand);
-        //randomise the skin of the ocelot used when spawning ocelots.
+        //randomise the type of the cat used when spawning.
         Random random = new Random();
         int min = 1;
         int max = 3;
         int randomSkinValue = random.nextInt((max - min) + 1) + min;
 
         //The position the player is looking, this is used as the position the ocelot spawns.
-        double x = player.rayTrace(30, 1).getBlockPos().getX();
-        double y = player.rayTrace(30, 1).getBlockPos().getY();
-        double z = player.rayTrace(30, 1).getBlockPos().getZ();
 
-        if (hand == EnumHand.MAIN_HAND) {
+            Vec3d vec3d = player.getEyePosition(1.0F);
+            Vec3d vec3d1 = player.getLook(1.0F);
+            Vec3d vec3d2 = vec3d.add(vec3d1.x * 30, vec3d1.y * 30, vec3d1.z * 30);
+            BlockRayTraceResult rayTraceResult = world.rayTraceBlocks(new RayTraceContext(vec3d, vec3d2, RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.NONE, player));
+
+            double x = rayTraceResult.getPos().getX();
+            double y = rayTraceResult.getPos().getY();
+            double z = rayTraceResult.getPos().getZ();
+
+        if (hand == Hand.MAIN_HAND) {
             if (!outOfUses(item)) {
 
                 if (!world.isRemote) {
-                    //TODO In 1.14 change ocelots to cats, they have been split into there own entity and ocelots can
-                    // no longer be tamed but gain trust for the player who feeds them fish.
-                    EntityOcelot ocelot = new EntityOcelot(world);
-                    ocelot.setLocationAndAngles(x, y + 1, z, player.rotationYaw, 0.0F);
-                    ocelot.setTamed(true);
-                    ocelot.setTamedBy(player);
-                    ocelot.setTameSkin(randomSkinValue);
-                    world.spawnEntity(ocelot);
+                    CatEntity cat = new CatEntity(EntityType.CAT, world);
+                    cat.setLocationAndAngles(x, y + 1, z, player.rotationYaw, 0.0F);
+                    cat.setTamed(true);
+                    cat.setTamedBy(player);
+                    cat.setCatType(randomSkinValue);
+                    world.addEntity(cat);
                 }
 
                 world.playSound(null, x, y, z, SoundEvents.ENTITY_EGG_THROW, SoundCategory.PLAYERS,
-                        0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+                        0.5F, 0.4F / (world.rand.nextFloat() * 0.4F + 0.8F));
 
-                if (!player.capabilities.isCreativeMode) {
-                    item.damageItem(1, player);
+                if (!player.isCreative()) {
+                    item.damageItem(1, player, (consumer) -> consumer.sendBreakAnimation(hand));
                     player.getCooldownTracker().setCooldown(this, 1200);
                 }
             } else {
                 world.playSound(null, x, y, z, SoundEvents.ENTITY_ITEM_BREAK, SoundCategory.PLAYERS,
-                        0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+                        0.5F, 0.4F / (world.rand.nextFloat() * 0.4F + 0.8F));
             }
         }
-
-        player.addStat(StatList.getObjectUseStats(this));
-        return new ActionResult<>(EnumActionResult.SUCCESS, item);
+        return new ActionResult<>(ActionResultType.SUCCESS, item);
     }
 }
